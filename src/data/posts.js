9533fae56fce0c23a9ccc0784bbd21d29db72e7f
Suhe <sdkaraarslan@gmail.com>
2023-02-27 00:20:24 +0100

    change the first two posts from mock data to real data

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,17 +1,19 @@
 export const posts = [
   {
-    title: "Test post 1",
+    title: "The inspiration and joy of life that comes with plants",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris convallis sodales leo et dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas nec nisl sodales neque elementum efficitur sit amet sit amet erat. Aenean ut maximus risus. Pellentesque sit amet tellus risus. Nam malesuada imperdiet arcu et ultrices. Phasellus sapien nulla, ultricies eu tempor eu, ultrices sed neque. Morbi tristique erat sit amet nisi congue, id pretium libero sodales. Morbi consequat malesuada enim eu aliquet. Mauris nec condimentum felis.",
-    imageUrl: "https://via.placeholder.com/320x180",
+      "In this post, we will discuss the importance of plants and how they play a crucial role in our lives. Plants are not only a source of food, but they also provide us with oxygen, which is essential for our survival. They help in reducing the amount of carbon dioxide in the atmosphere and contribute to maintaining the balance of nature.",
+    imageUrl: "/gallery/hobbies/first-post/p3-320-240.png",
     date: "2023-02-26T13:39:04.163Z",
+    link: "/blog/plants",
   },
   {
-    title: "Test post 2",
+    title: "Celebrating New Year's Eve",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris convallis sodales leo et dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas nec nisl sodales neque elementum efficitur sit amet sit amet erat. Aenean ut maximus risus. Pellentes",
-    imageUrl: "/gallery/hobbies/first-post/p1.jpeg",
+    imageUrl: "/gallery/travelling/first-post/germany-two-320-240.jpg",
     date: "2023-02-26T13:39:04.163Z",
+    link: "/blog/new-years-eve",
   },
   {
     title: "Test post 3",
@@ -71,6 +73,6 @@ export const posts = [
   },
 ];
 
-export function getPosts(limit = 6) {
+export function getPosts(limit = 8) {
   return posts.slice(0, limit);
 }
