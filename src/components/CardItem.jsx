35a7f29327e81cbdc063c435c09b22707cc94eb3
Suhe <sdkaraarslan@gmail.com>
2023-03-14 20:28:26 +0100

    add slider w/splidejs

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -2,6 +2,7 @@ export default function CardItem({ title, description, imageUrl, link }) {
   return (
     <a
       href={link}
+      style={{ height: "100%" }}
       className="flex flex-1 overflow-hidden rounded-2xl border-text border-2 min-w-[16rem] xl:min-w-[20rem] md:max-w-[20rem] xl:max-w-[24rem] transition-all duration-200 bg-bg hover:bg-white/50"
     >
       <div className="pb-6 flex flex-col">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import ProfileColumn from "../components/ProfileColumn";
 import CardItem from "../components/CardItem";
 import { getPosts } from "../data/posts";
 import { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 export default function Home() {
   let [posts, setPosts] = useState([]);
   useEffect(() => {
-    getPosts().then((result) => {
+    getPosts({ limit: 6 }).then((result) => {
       setPosts(result.data);
     });
   }, []);
@@ -14,17 +16,39 @@ export default function Home() {
   return (
     <>
       <ProfileColumn />
-      <div className="flex flex-row flex-wrap justify-start w-full gap-3 mt-3 max-w-[100vw]">
+      {/* <div className="flex flex-row flex-wrap justify-start w-full gap-3 mt-3 max-w-[100vw]"> */}
+      <Splide
+        options={{
+          breakpoints: {
+            640: {
+              perPage: 1,
+            },
+            800: {
+              perPage: 2,
+            },
+            999999: {
+              perPage: 3,
+            },
+          },
+          gap: 10,
+          pagination: false,
+        }}
+        className="mt-3"
+        aria-label="Blog posts"
+      >
         {posts.map((post) => (
-          <CardItem
-            key={post.id.toString()}
-            title={post.title}
-            description={post.description}
-            imageUrl={process.env.REACT_APP_ASSETS_PREFIX + post.thumbnail.id}
-            link={"/blog/" + post.category.slug + "/" + post.slug}
-          />
+          <SplideSlide className="flex flex-col items-center">
+            <CardItem
+              key={post.id.toString()}
+              title={post.title}
+              description={post.description}
+              imageUrl={process.env.REACT_APP_ASSETS_PREFIX + post.thumbnail.id}
+              link={"/blog/" + post.category.slug + "/" + post.slug}
+            />
+          </SplideSlide>
         ))}
-      </div>
+      </Splide>
+      {/* </div> */}
     </>
   );
 }
